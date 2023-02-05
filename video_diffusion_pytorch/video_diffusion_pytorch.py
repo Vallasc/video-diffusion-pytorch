d873a156a9818c960530ccf330a9a69b435fe5d1
Vallasc <giacomovallorani@protonmail.com>
2023-02-05 16:18:33 +0100

    Update

--- video_diffusion_pytorch/video_diffusion_pytorch.py
+++ video_diffusion_pytorch/video_diffusion_pytorch.py
@@ -948,6 +948,7 @@ class Trainer(object):
         while self.step < self.train_num_steps:
             for i in range(self.gradient_accumulate_every):
                 data = next(self.dl).cuda()
+                print(data)
                 with autocast(enabled = self.amp):
                     if self.use_path_as_cond:
                         loss = self.model(
